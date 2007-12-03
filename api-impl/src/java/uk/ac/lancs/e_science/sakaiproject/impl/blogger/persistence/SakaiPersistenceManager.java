1c3c6059df3b4ab77b640da2357ccc8dd67070db
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-12-03 09:42:12 +0000

    Took out an unused import and added the snapshot db api to the eclipse classpath.
    git-svn-id: https://source.sakaiproject.org/svn/blog/trunk@38943 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- api-impl/src/java/uk/ac/lancs/e_science/sakaiproject/impl/blogger/persistence/SakaiPersistenceManager.java
+++ api-impl/src/java/uk/ac/lancs/e_science/sakaiproject/impl/blogger/persistence/SakaiPersistenceManager.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
 
-import java.io.ByteArrayInputStream;
 import java.sql.*;
 
 import org.sakaiproject.db.api.SqlService;
